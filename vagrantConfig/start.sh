e637ce955114467aadc55c868825b6aaf54aa041
TJ Draper <tj@buzzingpixel.com>
2017-06-12 14:42:10 -0500

    Make sure NGINX restarts after NFS is mounted so it can pick up the NGINX conf

--- vagrantConfig/start.sh
+++ vagrantConfig/start.sh
@@ -6,3 +6,6 @@
 # it under the terms of the Apache License 2.0.
 # http://www.apache.org/licenses/LICENSE-2.0
 # ---------------------------------------------------------------------------
+
+# Make sure NGINX restarts after NFS is mounted so it can pick up the NGINX conf
+sudo service nginx restart;
